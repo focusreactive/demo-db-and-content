5a57d8277bfd7d4ec9962b248d784afea9fe1acb
Oleg Proskurin <regx@usul.su>
2024-02-09 12:50:23 +0700

    Switch to NPM

--- sanity/loader/VisualEditing.tsx
+++ sanity/loader/VisualEditing.tsx
@@ -34,7 +34,6 @@ export default function VisualEditing() {
   }, [router]);
   useEffect(() => {
     const disable = enableOverlays({
-      allowStudioOrigin,
       history: {
         subscribe: (navigate) => {
           setNavigate(() => navigate);
